1619730536a52b54449cc4acdc7afbbeb28c9103
CodeShadow19 <alhasanofficial.asib@gmail.com>
2021-05-24 06:29:16 +0600

    After Basic Setup.

---
+++ BackEnd/resources/views/Home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('content')
+
+
+
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        
+    </script>
+@endsection
---
+++ FrontEnd/resources/views/Home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('content')
+
+    @include('component.Banner')
+
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+
+    </script>
+@endsection
