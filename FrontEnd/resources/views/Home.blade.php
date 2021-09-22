be9e8eb38f48cd07aa555d11e35ae1d7ae7e9c5b
CodeShadow19 <alhasanofficial.asib@gmail.com>
2021-09-22 09:05:09 +0600

    Windows!
    Windows!

--- FrontEnd/resources/views/Home.blade.php
+++ FrontEnd/resources/views/Home.blade.php
@@ -2,6 +2,15 @@
 @section('content')
 
     @include('component.Banner')
+    @include('component.Intro')
+    @include('component.Team')
+    @include('component.Counter')
+    @include('component.Services')
+    @include('component.Cta')
+    @include('component.Testimonial')
+    @include('component.LatestProject')
+    @include('component.Contact')
+    @include('component.Footer')
 
 @endsection
 
